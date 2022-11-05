c3442900196dcd0e3da69533bde181f5c29e4040
Thelma-Dev <osifoaisosa@gmail.com>
2022-11-05 17:10:48 -0500

    Update validation

--- assets/script/index.js
+++ assets/script/index.js
@@ -28,27 +28,10 @@ function readWindow() {
     number.value = '';
 } 
 
-
+// Add event listener
 onEvent('focus', inputField, function(event) {;
     guesses.classList = 'h2';
 });
-
-
-function isNumber(str) {
-    let input = str.trim();
-
-    if (input.length > 0 && !isNaN(input)) 
-        return true;
-
-    return false;
-    
-}
-
-
-
-const randomNumber = Math.floor(Math.random() * 50);
-
-console.log(randomNumber);
 
 
 onEvent('click', restartBtn, function() {
@@ -61,35 +44,53 @@ onEvent('keypress', inputField, function(event) {;
     if (event.key == "Enter") {
         event.preventDefault();
         btn.click();
-    };
-   
+    }; 
 });
 
 
+// Generating random number
+const randomNumber = Math.floor(Math.random() * 50);
+console.log(randomNumber);
+
+
+// Validating input
+function isValid(input) {
+    if (Number.isInteger(input)) {
+        return true;
+    }
+
+    return false;
+}
+
 let count = 1;
-
-// Adding an event listener
-
-onEvent('click', btn, function() {
+function validate() {
     let num = number.value.trim();
     let answer = randomNumber;
+    let valid = true;
 
-    if (isNumber(num) && num < answer) {
+    
+    if (num.length === 0 && !isValid(Number(num))) {
+        output.innerText = 'Enter a valid number';
+        valid = false;
+    } else if (num < answer) {
         let result = parseFloat(num);
-        output.innerText = `Oops! Try a bigger number `;
-  
-    } else {
-        if (num > answer) {
-            output.innerText = 'Oops! Try a smaller number';
-        } else {
-            output.innerText =`Woohoo! You guessed right`;
-            guesses.classList = 'is-visible';
-            guesses.innerText = `You had ${count} guesses`;
-            restartBtn.classList = 'is-visible';
-            number.value = '';
-            count = 0;
-        }
+        output.innerText = 'Oops! Try a bigger number';
+    } else if (num > answer) {
+        output.innerText = 'Oops! Try a smaller number';
+    } else if (num == answer) {
+        output.innerText ='Woohoo! You guessed right';
+        guesses.classList = 'is-visible';
+        guesses.innerText = `You had ${count} guesses`;
+        restartBtn.classList = 'is-visible';
+        number.value = '';
+        count = 0;
     }
     count++;
+}
+
+
+// Adding an event listener
+onEvent('click', btn, function() {
+    validate();
 });
 
